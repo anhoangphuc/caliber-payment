0637cd25cc84b2de26a4d9a1d942eea7183e082f
anhoangphuc <hoangphucnb97@gmail.com>
2025-01-11 14:54:41 +0700

    Refactor instructions structure folder

---
+++ programs/caliber-payment/src/instructions/admin/mod.rs
@@ -0,0 +1,9 @@
+pub mod admin_add_allowed_token;
+pub mod admin_initialize;
+pub mod admin_update_enabled_status;
+pub mod admin_update_oracle;
+
+pub use admin_add_allowed_token::*;
+pub use admin_initialize::*;
+pub use admin_update_enabled_status::*;
+pub use admin_update_oracle::*;
--- programs/caliber-payment/src/lib.rs
+++ programs/caliber-payment/src/lib.rs
@@ -15,27 +15,27 @@ pub mod caliber_payment {
     use super::*;
 
     pub fn admin_initialize(ctx: Context<AdminInitialize>, protocol_fee_rate: u16) -> Result<()> {
-        admin_initialize::handler(ctx, protocol_fee_rate)
+        admin::admin_initialize::handler(ctx, protocol_fee_rate)
     }
 
     pub fn admin_add_allowed_token(
         ctx: Context<AdminAddAllowedToken>,
         feed_id_hex: String,
     ) -> Result<()> {
-        admin_add_allowed_token::handler(ctx, &feed_id_hex)
+        admin::admin_add_allowed_token::handler(ctx, &feed_id_hex)
     }
 
     pub fn admin_update_allowed_token_enabled_status(
         ctx: Context<AdminUpdateEnabledStatus>,
         enabled: bool,
     ) -> Result<()> {
-        admin_update_enabled_status::handler(ctx, enabled)
+        admin::admin_update_enabled_status::handler(ctx, enabled)
     }
 
     pub fn admin_update_allowed_token_oracle(
         ctx: Context<AdminUpdateOracle>,
         feed_id_hex: String,
     ) -> Result<()> {
-        admin_update_oracle::handler(ctx, &feed_id_hex)
+        admin::admin_update_oracle::handler(ctx, &feed_id_hex)
     }
 }
